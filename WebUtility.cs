f5c046b69a3c97eb5d3ec8dc1508bff1076f8578
qingliuyu <qingliuyu@gmail.com>
2012-01-31 17:20:14 +0800

    WebUtility共通类

---
+++ WebUtility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Common
+{
+    public class WebUtility
+    {
+        #region GetParamValue
+        /// <summary>
+        /// 获取Http请求的参数值
+        /// </summary>
+        /// <param name="paramName">参数名</param>
+        /// <param name="defaultValue">默认参数值</param>
+        /// <returns>返回整形值，如页码，ID等</returns>
+        public static int GetParamValue(string paramName, int defaultValue)
+        {
+            HttpRequest request = HttpContext.Current.Request;
+            string method = request.HttpMethod.ToLower();
+            int ret = defaultValue;
+            string value = method.Equals("get") ? request.QueryString[paramName] : request[paramName];
+            if (!string.IsNullOrEmpty(value))
+            {
+                ret = int.Parse(value);
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// 获取Http请求的参数值
+        /// </summary>
+        /// <param name="paramName">参数名</param>
+        /// <param name="defaultValue">默认参数值</param>
+        /// <returns></returns>
+        public static string GetParamValue(string paramName, string defaultValue)
+        {
+            HttpRequest request = HttpContext.Current.Request;
+            string method = request.HttpMethod.ToLower();
+            string ret = defaultValue;
+            // method = "get"，数据保存在Http请求头中
+            // mehtod = "post"，数据保存在Http请求体中
+            string value = method.Equals("get") ? request.QueryString[paramName] : request[paramName];
+            if (!string.IsNullOrEmpty(value))
+            {
+                ret = value;
+            }
+            return ret;
+        }
+        #endregion
+    }
+}
